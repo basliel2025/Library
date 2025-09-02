63cc29dcb1b900115b4a09d40ff448f8b6bafaf9
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-09-03 00:10:01 +0300

    replaced plain costructor with class

--- library.js
+++ library.js
@@ -1,11 +1,13 @@
 const myLibrary =[];
 const shelf = document.getElementById('shelf');
-function Book(title , author , pages , read){
+class Book{
+    constructor(title , author , pages , read){
     this.title = title;
     this.author= author;
     this.pages = pages;
     this.read = read;
     this.id= crypto.randomUUID();
+    }
 }
 function addBookToLibrary(book){
 myLibrary.push(book);
